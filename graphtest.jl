b725797c48db98f5d3990a0c05cd96f3e45fc4e0
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2022-11-20 15:19:22 +0100

    Update run.jl graphtest.jl

--- graphtest.jl
+++ graphtest.jl
@@ -5,14 +5,18 @@ using GLMakie, GraphMakie, NetworkLayout
 ## -- Test -- ##
 ## Run a small simulation and plot the tree twice side by side 
 ## annotated with A and C respectively.
-Pbd = birthdeath(1, 32^2, 0.9; N=0)
+Pbd = birthdeath(1, 32^2, 0.9; N=0)[1]
 Pbal = maximally_unbalanced(6)
 
+Abd,Cbd = treevalues!(Pbd)
+Abal,Cbal = treevalues!(Pbal)
+
+
 fig = Figure()
-graphplot(fig[1,1], reverse(Pbd), nlabels=repr.(A(Pbd)), layout=Buchheim())
-graphplot(fig[1,2], reverse(Pbd), nlabels=repr.(C(Pbd)), layout=Buchheim())
-graphplot(fig[2,1], reverse(Pbal), nlabels=repr.(A(Pbal)), layout=Buchheim())
-graphplot(fig[2,2], reverse(Pbal), nlabels=repr.(C(Pbal)), layout=Buchheim())
+graphplot(fig[1,1], SimpleDiGraph(adjacency_matrix(Pbd)), nlabels=repr.(Abd), layout=Buchheim())
+graphplot(fig[1,2], (Pbd), nlabels=repr.(Cbd), layout=Buchheim())
+graphplot(fig[2,1], reverse(Pbal), nlabels=repr.(Abal), layout=Buchheim())
+graphplot(fig[2,2], reverse(Pbal), nlabels=repr.(Cbal), layout=Buchheim())
 display(fig)
 ## ---------- ##
 #
--- run.jl
+++ run.jl
@@ -1,6 +1,12 @@
-include("moran_vs_coalescent.jl")
+push!(LOAD_PATH, "./TreeProcesses/src/")
+push!(LOAD_PATH, "./BinaryTrees/src/")
+using BinaryTrees, TreeProcesses
+using CategoricalArrays, DataFrames, DataFramesMeta, StatsBase
+using JLD2
 
 using GLMakie
+
+n = 2^13
 d = 0.975
 
 ## Aggregate A,C over multiple coalescent runs.
@@ -18,7 +24,7 @@ dfcoal = to_mean_dataframe(Acoal, Ccoal)
 Abd = Int[]
 Cbd = Int[]
 @time for _ in 1:50
-  global Pbd = birthdeath(1, ceil(Int, 1/(1-d))*n, d; N=0)
+  Pbd = birthdeath(1, ceil(Int, 1/(1-d))*n, d; N=0)
   _A, _C = treevalues!(Pbd[1])
   append!(Abd, _A)
   append!(Cbd, _C)
@@ -28,8 +34,8 @@ dfbd = to_mean_dataframe(Abd,Cbd)
 ## Aggregate A,C over multiple fluctuating_coalescent runs.
 Afc = Int[]
 Cfc = Int[]
-@time for _ in 1:100
-  global Pfc = fluctuating_coalescent(n)
+@time for _ in 1:50
+  Pfc = fluctuating_coalescent(n; fuse=max)
   _A, _C = treevalues!(Pfc)
   append!(Afc, _A)
   append!(Cfc, _C)
@@ -86,3 +92,109 @@ begin
 
   current_figure()
 end
+
+##--------------------------------------##
+
+
+dffc_eta = load("data/03.jld2", "dffc_eta")
+
+using ThreadsX, StatsBase
+import Base.Iterators: product
+etas = [0.0,2.0,3.0,6.0,12.0]
+etas = 1.2 #1.2:0.005:(1.3-0.005)
+etas = [1.0, 1.2, 1.3, 1.4, 2.0]
+ns = 2 .^[8,10,14,16,18]
+dffc_eta = DataFrame()
+(map(product(etas, ns)) do (η,n)
+  Afc = Int[]
+  Cfc = Int[]
+  if η==0.0
+    fuse = max
+  else
+    fuse=(x,y)->(x+y)/η
+  end
+  lck = ReentrantLock()
+  @time Threads.@threads for _ in 1:300
+    Pfc = fluctuating_coalescent(n; fuse)
+    _A, _C = treevalues!(Pfc)
+    lock(lck)
+    try
+      append!(Afc, _A)
+      append!(Cfc, _C)
+    finally
+      unlock(lck)
+    end
+  end
+  flush(stdout)
+  dffc = to_mean_dataframe(Afc,Cfc)
+  dffc.eta .= η
+  dffc.n .= n
+  append!(dffc_eta, dffc, cols=:union)
+end)
+
+
+begin
+  plot_etas = [collect(1.2:0.005:1.4)[1:1:end]; [0.0,0.5,1.0,2.0]]
+  plot_etas = 1.37:0.005:1.38
+  plot_etas = [1.37,1.375,1.2, 1.0]
+  plot_etas = levels(dffc_eta.eta)
+  plot_etas = [1.0, 2.0]
+  etac = 1.375
+  α = 0.0#1/3
+  β = 1#1.54 #1.515
+  @transform!(dffc_eta, :coverabeta = :covera ./ :a.^(β-1))
+  if α===-0.0
+    @transform!(dffc_eta, :x = @. -1/log(abs((:eta-etac)/etac))*:a )
+  else
+    @transform!(dffc_eta, :x = abs.((:eta.-etac)/etac).^α .* :a )
+  end
+  # @transform!(dffc_eta, :x = :a )
+  dfsub = @subset(dffc_eta, :eta .∈ Ref(plot_etas), :x.>=0, :x.<Inf)
+  @transform!(dfsub, :x = abs.(:x))
+  bw = 1/200
+  bins = 10.0.^range(log10.(extrema(dfsub.x).+eps())..., step=bw)
+  dfsub.xbin = cut(dfsub.x, bins[2:end-1], labels=midpoints(bins), extend=true)|>Array
+  cdfsub = combine(groupby(dfsub, [:n, :eta, :xbin]), :coverabeta => mean => :coverabeta)
+end
+begin
+  fig2 = Figure(resolution=72 .*(11,8), fontsize=12)
+  ax3 = Axis(fig2[1,1], xscale=log2, yscale=log2, backgroundcolor=:lightgray)
+
+  etas = levels(cdfsub.eta)
+  etamin, etamax = extrema(etas)
+  colors = cgrad(:RdYlGn_11)
+  for d in groupby(cdfsub, [:n, :eta])
+    eta = d[1, :eta]
+    n = d[1, :n]
+    #d = @subset(cdfsub, :eta.==eta)
+    if eta == etac
+      marker=:rect
+    else
+      marker=:circle
+    end
+    Makie.scatter!(ax3, d.xbin, d.coverabeta, markersize=8, label="$eta",
+     #color=get(colors, (eta-etac)/(1.5) + 1/2), marker=marker
+     )
+  end
+  xlims!(ax3, 1, 1e5)
+  ylims!(ax3, 1e-1, 1e2)
+  Legend(fig2[1,2], ax3, nbanks=2, bgcolor=:lightgray)
+  # begin
+  #   x1, y1 = 4, 14.2
+  #   x0, y0 = 3, 10.5
+  #   m = y1-y0
+  #   lines!(ax3, 1e1..1e5, x->m*log10(x)+((y1+y0)-m*(x1+x0))/2, color=:black, linewidth=4,linestyle=:dash)
+  # end
+  # begin
+  #   x1, y1 = 4, 52
+  #   x0, y0 = 3, 25
+  #   m = y1-y0
+  #   lines!(ax3, 2e2..1e5, x->m*log10(x)+((y1+y0)-m*(x1+x0))/2, color=:black, linewidth=4,linestyle=:dash)
+  # end
+  # lines!(ax3, 1e0..1e3, x->x^(0.50), linewidth=4, linestyle=:dot)
+  # lines!(ax3, 1e2..1e4, x->10^(-1/2)*x^(1.0), linewidth=4, linestyle=:dot)
+  # hlines!(ax3, 10^-(0.04))
+  lines!(ax3, 1e0..1e5, x->x^(0.515))
+  # lines!(ax3, 1e2..1e5, x->0.9*(1e2/x)^(0.25))
+  fig2
+end 
